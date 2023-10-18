737cf76aff3d728608677b49db1be20f56de3cba
[SW4/2반] 노은탁 <popcron13@gmail.com>
2023-10-18 21:39:29 +0900

    portfolio first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: `Netpo : Welcome!`,
+  description: `Netpo's portfolio page`,
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
