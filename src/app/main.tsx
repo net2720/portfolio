c7b05efbc177004d2bd8bbb4634fd2e90cdc6418
[SW4/2반] 노은탁 <popcron13@gmail.com>
2023-10-20 21:21:51 +0900

    feat / 일부 CSS 추가 커밋

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,7 +3,21 @@ import './main.css';
 export default function Main() {
   return (
     <>
-      <div className="main">인삿말</div>
+      <div className="main">
+        <h1 className="mainTag">
+          노은탁
+          <br />
+          portfolio
+        </h1>
+        <p className="bar">-</p>
+        <p className="introduce">
+          안녕하세요!
+          <br />
+          소통과 협력을 가장 중요하게 여기는 신입 개발 희망자입니다.
+          <br />
+          하겠다고 약속한 말은 무조건 지키는 책임감을 모토로 삼고 있습니다.
+        </p>
+      </div>
     </>
   );
 }
