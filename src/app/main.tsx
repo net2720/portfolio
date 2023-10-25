75ba94483937e3bf8b9de9f3f8de83cee2760174
[SW4/2반] 노은탁 <popcron13@gmail.com>
2023-10-25 19:35:09 +0900

    fix / main 부분 CSS 수정 커밋

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -9,14 +9,14 @@ export default function Main() {
           <br />
           portfolio
         </div>
-        <p className="bar">-</p>
-        <p className="introduce">
+        <div className="bar"></div>
+        <div className="introduce">
           안녕하세요!
           <br />
           소통과 협력을 가장 중요하게 여기는 신입 개발 희망자입니다.
           <br />
           하겠다고 약속한 말은 무조건 지키는 책임감을 모토로 삼고 있습니다.
-        </p>
+        </div>
       </div>
     </>
   );
